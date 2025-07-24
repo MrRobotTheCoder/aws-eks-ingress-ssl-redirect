006216afb7ad78f497db5c67c7753708da646a90
Abinash Sethi <sethiabinash123@gmail.com>
2025-07-24 00:32:15 -0400

    initial commit

---
+++ 4-ingress-ssl-tf-manifests/11-acm-certificate.tf
@@ -0,0 +1,25 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "*.hellosaanvika.com"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "dev"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+    }
+}
+
+# Outputs
+output "acm_certificate_id" {
+  value = aws_acm_certificate.acm_cert.id 
+}
+
+output "acm_certificate_arn" {
+  value = aws_acm_certificate.acm_cert.arn
+}
+
+output "acm_certificate_status" {
+  value = aws_acm_certificate.acm_cert.status
+}
